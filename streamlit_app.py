f751cbd3d318cdcbe6cd539c5ecfb2b114d78983
monicasnowflake <131186404+monicasnowflake@users.noreply.github.com>
2023-04-18 12:20:06 -0400

    Update streamlit_app.py
    Updated header and added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
-import streamlit
+dimport streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
-streamlit.title ('Breakfast Menu')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
+streamlit.text ('Hard-Boiled Dree Range Egg')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
